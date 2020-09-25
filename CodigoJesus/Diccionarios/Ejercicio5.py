83093b8a5c5bfaf5d23ae2bc26abd57658676752
Jesus Garcia <xexugarcia95@gmail.com>
2020-09-26 01:52:10 +0200

    más ejercicios de diccionarios

---
+++ CodigoJesus/Diccionarios/Ejercicio5.py
@@ -0,0 +1,18 @@
+"""Escribir un programa que almacene el diccionario con los créditos de las
+asignaturas de un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después
+muestre por pantalla los créditos de cada asignatura en el formato <asignatura>
+tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del
+curso, y <créditos> son sus créditos. Al final debe mostrar también el número
+total de créditos del curso."""
+
+diccionario = {"matematicas":6,"fisica":4,"quimica":5}
+lista = list(diccionario)
+
+for key in diccionario.keys():
+    print(str(key) + " tiene " + str(diccionario.get(key)) + " creditos")
+
+cont = 0
+for value in diccionario.values():
+    cont+=value
+
+print("El total de créditos es " + str(cont))
---
+++ CodigoJesus/Diccionarios/Ejercicio6.py
@@ -0,0 +1,13 @@
+"""Escribir un programa que cree un diccionario vacío y lo vaya llenado con
+información sobre una persona (por ejemplo nombre, edad, sexo, teléfono,
+correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un
+nuevo dato debe imprimirse el contenido del diccionario."""
+
+diccionario = {}
+clave = input("Introduce clave: ")
+while clave !='.':
+    info = input("Introduce valor: ")
+    diccionario[clave] = info
+    print(diccionario)
+    clave = input("Introduce clave: ")
+print("Fin")
