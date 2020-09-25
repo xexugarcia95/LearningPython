59eb48c0f4287b5627a51f666e4f0c643fc86b45
Jesus Garcia <xexugarcia95@gmail.com>
2020-09-25 20:22:28 +0200

    Ejercicios de diccionarios, faltan más

--- CodigoJesus/Diccionarios/Ejercicio1.py
+++ CodigoJesus/Diccionarios/Ejercicio1.py
@@ -1,1 +1,8 @@
-#Empezar a hacer los ejercicios...
+"""Escribir un programa que guarde en una variable el diccionario
+{'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y
+muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario."""
+
+diccionario = {"Euro":"€","Dollar":"$","Yen":"¥"}
+divisa = input("Divisa que le interesa: ")
+
+print(diccionario.get(divisa,"No existe"))
---
+++ CodigoJesus/Diccionarios/Ejercicio2.py
@@ -0,0 +1,17 @@
+"""Escribir un programa que pregunte al usuario su nombre, edad, dirección
+y teléfono y lo guarde en un diccionario. Después debe mostrar por pantalla
+el mensaje <nombre> tiene <edad> años, vive en <dirección> y su número de
+teléfono es <teléfono>."""
+
+diccionario = {}
+aux = input("Introduce un nombre: ")
+diccionario["nombre"] = aux
+aux = input("Introduce tu edad: ")
+diccionario["edad"] = aux
+aux = input("Introduce tu dirección: ")
+diccionario["direccion"] = aux
+aux = input("Teléfono: ")
+diccionario["telefono"] = aux
+
+print(str(diccionario.get("nombre")) + " tiene " + str(diccionario.get("edad")) + " años, vive en "
++ str(diccionario.get("direccion")) + " y su número de teléfono es " + str(diccionario.get("telefono")))
