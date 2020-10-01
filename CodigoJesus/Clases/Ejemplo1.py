546b097f78c4c5c136f40f4e198bbfb2e8d14f2e
Jesus Garcia <xexugarcia95@gmail.com>
2020-10-01 23:58:58 +0200

    unos pocos cambios, constructor, encapsulacion

--- CodigoJesus/Clases/Ejemplo1.py
+++ CodigoJesus/Clases/Ejemplo1.py
@@ -1,24 +1,34 @@
 class Coche():
-    largoChasis = 250
-    anchoChasis = 120
-    ruedas = 4
-    enMarcha = False
+
+    def __init__(self):
+        self.__largoChasis = 250
+        self.__anchoChasis = 120
+        self.__ruedas = 4  # Encapsulación --> privada
+        self.enMarcha = False
 
     # Para ser un método en vez de función, debe tener self
-    def arrancar(self):
-        self.enMarcha = True
+    def arrancar(self, arrancamos):
+        self.enMarcha = arrancamos
+
+        if self.enMarcha:
+            return "El coche está en marcha"
+        else:
+            return "El coche está parado"
 
     def estado(self):
-        if self.enMarcha:
-            print("El coche está en marcha")
-        else:
-            print("El coche está parado")
+        print("El coche tiene", self.__ruedas, "ruedas, un ancho de",
+              self.__anchoChasis, "y un largo de", self.__largoChasis)
 
 
 miCoche = Coche()
-miCoche.estado()
-print("El largo del coche es: ", miCoche.largoChasis)
-print("El coche tiene", miCoche.ruedas, "ruedas")
 
-miCoche.arrancar()
+print(miCoche.arrancar(True))
 miCoche.estado()
+
+print("A continuacion creamos el segundo coche")
+
+miCoche2 = Coche()
+print(miCoche2.arrancar(False))
+
+miCoche2.__ruedas = 5
+miCoche2.estado()
