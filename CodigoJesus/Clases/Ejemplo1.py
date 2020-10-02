212550be8b96392c644d258f9dc888bb3c29a74d
Jesus Garcia <xexugarcia95@gmail.com>
2020-10-02 18:42:42 +0200

    nuevos cambios, métidos privados

--- CodigoJesus/Clases/Ejemplo1.py
+++ CodigoJesus/Clases/Ejemplo1.py
@@ -4,14 +4,19 @@ class Coche():
         self.__largoChasis = 250
         self.__anchoChasis = 120
         self.__ruedas = 4  # Encapsulación --> privada
-        self.enMarcha = False
+        self.__enMarcha = False
 
     # Para ser un método en vez de función, debe tener self
     def arrancar(self, arrancamos):
-        self.enMarcha = arrancamos
+        self.__enMarcha = arrancamos
 
-        if self.enMarcha:
+        if self.__enMarcha:
+            chequeo = self.__chequeo_interno()
+
+        if self.__enMarcha and chequeo:
             return "El coche está en marcha"
+        elif self.__enMarcha and not chequeo:
+            return "Algo ha ido mal en el chequeo, no se puede arrancar"
         else:
             return "El coche está parado"
 
@@ -19,16 +24,28 @@ class Coche():
         print("El coche tiene", self.__ruedas, "ruedas, un ancho de",
               self.__anchoChasis, "y un largo de", self.__largoChasis)
 
+    def __chequeo_interno(self):
+        print("Realizando chequeo interno...")
+        self.gasolina = "ok"
+        self.aceite = "ok"
+        self.puertas = "cerradas"
+
+        if (self.gasolina == "ok" and self.aceite == "ok"
+                and self.puertas == "cerradas"):
+            return True
+        else:
+            return False
+
 
 miCoche = Coche()
 
 print(miCoche.arrancar(True))
 miCoche.estado()
 
+
 print("A continuacion creamos el segundo coche")
 
 miCoche2 = Coche()
 print(miCoche2.arrancar(False))
 
-miCoche2.__ruedas = 5
 miCoche2.estado()
